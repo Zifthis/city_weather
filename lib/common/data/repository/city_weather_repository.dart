d56cc2a0b8edc37e5d0d4c3f60114ec5cd196f71
matija <matija.kovacevic@q.agency>
2022-11-25 12:52:55 +0100

    repo fix

--- lib/common/data/repository/city_weather_repository.dart
+++ lib/common/data/repository/city_weather_repository.dart
@@ -1,23 +1,38 @@
 import 'package:city_weather/common/data/api_client/api_client.dart';
 import 'package:city_weather/common/data/provider.dart';
+import 'package:city_weather/common/data/repository/i_city_weather_repository.dart';
 import 'package:city_weather/common/domain/error_handling/app_failure.dart';
 import 'package:city_weather/common/domain/error_handling/either_failure_or.dart';
-import 'package:city_weather/feature/weather/data/repository/i_weather_repository.dart';
+import 'package:city_weather/feature/search/domain/entities/search.dart';
 import 'package:city_weather/feature/weather/domain/entities/weather.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final weatherRepositoryProvider = Provider<IWeatherRepository>(
-  (ref) => WeatherRepository(
+final cityWeatherRepositoryProvider = Provider<ICityWeatherRepository>(
+  (ref) => WeatherRepositoryProvider(
     ref.read(apiClientProvider),
   ),
 );
 
-class WeatherRepository implements IWeatherRepository {
+class WeatherRepositoryProvider implements ICityWeatherRepository {
   final ApiClient _apiClient;
 
-  WeatherRepository(this._apiClient);
+  WeatherRepositoryProvider(this._apiClient);
+
+  @override
+  EitherAppFailureOr<List<Search>> fetchSearchResponse(String search) async {
+    try {
+      final response = await _apiClient.getSearchResults(search);
+      return Right(response.map((e) => e.toDomain()).toList());
+    } on DioError catch (error) {
+      final err = error;
+      err.response?.statusCode;
+      return Left(AppFailure.fromDioErrorResponse(err));
+    } catch (err) {
+      return Left(Failure.serverError.toAppFailure);
+    }
+  }
 
   @override
   EitherAppFailureOr<Weather> fetchWeatherResponse(String cityName) async {
--- lib/common/data/repository/i_city_weather_repository.dart
+++ lib/common/data/repository/i_city_weather_repository.dart
@@ -1,6 +1,8 @@
 import 'package:city_weather/common/domain/error_handling/either_failure_or.dart';
 import 'package:city_weather/feature/search/domain/entities/search.dart';
+import 'package:city_weather/feature/weather/domain/entities/weather.dart';
 
-abstract class ISearchRepository {
+abstract class ICityWeatherRepository {
   EitherAppFailureOr<List<Search>> fetchSearchResponse(String search);
+  EitherAppFailureOr<Weather> fetchWeatherResponse(String cityName);
 }

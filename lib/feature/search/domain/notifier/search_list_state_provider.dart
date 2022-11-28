a878c8b4067e4f4f0c503207e92c895a83c2f4bc
matija <matija.kovacevic@q.agency>
2022-11-28 11:27:43 +0100

    removing unused files

--- lib/feature/search/domain/notifier/search_list_state_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final searchListStateProvider = Provider<List<String>>(
-  (ref) {
-    List<String> userLocationList = [];
-    return userLocationList;
-  },
-);

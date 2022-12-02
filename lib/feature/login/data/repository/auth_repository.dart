fdef7a99bb4ae05300417d825e8668937e52279d
matija <matija.kovacevic@q.agency>
2022-12-02 13:48:22 +0100

    repo fix

--- lib/feature/login/data/repository/auth_repository.dart
+++ lib/feature/login/data/repository/auth_repository.dart
@@ -1,4 +1,3 @@
-import 'package:city_weather/common/domain/error_handling/auth_exceptions.dart';
 import 'package:city_weather/feature/login/data/repository/i_auth_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -12,32 +11,43 @@ class AuthRepository implements IAuthRepository {
   AuthRepository(this._auth);
 
   @override
-  Future<User?> signIn(String email, String password) async {
+  Future<void> signIn(
+    String email,
+    String password,
+  ) async {
     try {
-      final auth = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return auth.user;
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
-        throw AuthException('User not found');
+        throw Exception('No user found for that email.');
       } else if (e.code == 'wrong-password') {
-        throw AuthException('Wrong password');
-      } else {
-        throw AuthException('An error occurred. Please try again later');
+        throw Exception('Wrong password provided for that user.');
       }
     }
   }
 
   @override
-  Future<void> signOut() async {
-    await _auth.signOut();
+  Future<void> signUp(String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw Exception('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw Exception('The account already exists for that email.');
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
   @override
-  Stream<User?> userChanged() {
-    final token = _auth.idTokenChanges();
-    return token;
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      throw Exception(e);
+    }
   }
 }
--- lib/feature/login/data/repository/i_auth_repository.dart
+++ lib/feature/login/data/repository/i_auth_repository.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
 abstract class IAuthRepository {
-  Future<User?> signIn(String email, String password);
+  Future<void> signIn(String email, String password);
+  Future<void> signUp(String email, String password);
   Future<void> signOut();
-  Stream<User?> userChanged();
 }
